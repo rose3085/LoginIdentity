58ed6e8c4b52f45a5f79e8c76233fe08ded53f4a
rose3085 <rabinasedhai1@gmail.com>
2023-12-06 01:03:11 +0545

    role based access get method

--- LoginIdentity/Controllers/WeatherForecastController.cs
+++ LoginIdentity/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using LoginIdentity.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LoginIdentity.Controllers
@@ -9,25 +11,41 @@ namespace LoginIdentity.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [HttpGet]
+        [Route("GetUnAuthorized")]
+        
+        public IActionResult Get()
         {
-            _logger = logger;
+            return Ok(Summaries);
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+
+        [HttpGet]
+        [Route("GetByUserRole")]
+        [Authorize(Roles = StaticUserRoles.USER)]
+        public IActionResult GetUserRole()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
+        }
+
+
+        [HttpGet]
+        [Route("GetByAdminRole")]
+        [Authorize(Roles = StaticUserRoles.ADMIN)]
+        public IActionResult GetAdminRole()
+        {
+            return Ok(Summaries);
+        }
+
+
+        [HttpGet]
+        [Route("GetByOwner")]
+        [Authorize(Roles =StaticUserRoles.OWNER)]
+        public IActionResult GetOwnerRole()
+        {
+            return Ok(Summaries);
         }
     }
 }
